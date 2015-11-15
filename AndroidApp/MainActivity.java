91acb2d42c573a6cb4311a88caa820d6ee47be0c
Itaf Omar Joudeh <itaf.on.line@hotmail.com>
2015-11-14 21:59:03 -0500

    Update MainActivity.java

--- AndroidApp/MainActivity.java
+++ AndroidApp/MainActivity.java
@@ -1,24 +1,50 @@
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button, button1;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addListenerOnButton();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        addListenerOnButton1();
         addListenerOnButton2();
     }
 
-    public void addListenerOnButton()
+    /* Button Listeners */
+
+    //Login
+    public void addListenerOnButton1()
+    {
+        final Context context = this;
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(context, Connection.class/*Monitoring.class*/);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //New User
+    public void addListenerOnButton2()
     {
         final Context context = this;
         button = (Button) findViewById(R.id.button2);
@@ -27,22 +53,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View arg0) {
                 Intent intent = new Intent(context, NewUser.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    public void addListenerOnButton2()
-    {
-        final Context context = this;
-        button1 = (Button) findViewById(R.id.button1);
-
-        button1.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View arg0)
-            {
-                Intent intent = new Intent(context, Monitoring.class);
                 startActivity(intent);
             }
         });
