cc3387983d981a984b0388a14f2bdf91d0998745
Itaf Omar Joudeh <itaf.on.line@hotmail.com>
2015-11-14 22:01:07 -0500

    Update NewUser.java

--- AndroidApp/NewUser.java
+++ AndroidApp/NewUser.java
@@ -1,11 +1,16 @@
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class NewUser extends AppCompatActivity {
-
+public class NewUser extends AppCompatActivity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
+
